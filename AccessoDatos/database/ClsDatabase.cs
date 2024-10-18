2e95987b1db4300df0d3f54dbcb1d7f4b13d9068
Jurgen <jurgenq75@gmail.com>
2024-10-18 14:13:57 -0600

    Add project files.

---
+++ AccessoDatos/database/ClsDatabase.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace AccessoDatos.database
+{
+
+    public class ClsDatabase
+    {
+        #region variables privadas
+
+        private SqlConnection _objSqlConnection;
+        private SqlDataAdapter _SqlDataAdapter;
+        private SqlCommand _SqlCommand;
+        private DataSet _resultado;
+        private DataTable _parametros;
+
+        private string nombreTabla, nombreSP, mensajeErrorDB, valorEscalar, nombreDB;
+        private bool escalar;
+
+        #endregion
+
+        #region Variables publicas
+
+        public SqlConnection ObjSqlConnection { get => _objSqlConnection; set => _objSqlConnection = value; }
+        public SqlDataAdapter SqlDataAdapter { get => _SqlDataAdapter; set => _SqlDataAdapter = value; }
+        public SqlCommand SqlCommand { get => _SqlCommand; set => _SqlCommand = value; }
+        public DataSet Resultado { get => _resultado; set => _resultado = value; }
+        public DataTable Parametros { get => _parametros; set => _parametros = value; }
+        public string NombreTabla { get => nombreTabla; set => nombreTabla = value; }
+        public string NombreSP { get => nombreSP; set => nombreSP = value; }
+        public string MensajeErrorDB { get => mensajeErrorDB; set => mensajeErrorDB = value; }
+        public string ValorEscalar { get => valorEscalar; set => valorEscalar = value; }
+        public string NombreDB { get => nombreDB; set => nombreDB = value; }
+        public bool Escalar { get => escalar; set => escalar = value; }
+        #endregion
+
+
+
+    }
+}
---
+++ LogicaNegocio/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LogicaNegocio")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LogicaNegocio")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ae2b2300-aae6-49a1-86a1-219b3470c412")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
